30e2a25630052349ab7e9a4a220bd68606b43e98
Emil Andersson <emil.andersson20@learnet.se>
2020-01-09 12:58:06 +0100

    Add project files.

---
+++ Coskrrak/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Coskrrak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
